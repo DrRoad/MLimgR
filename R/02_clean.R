f5218de1aab8322fe0d3b344e2a6479d6c40a8a9
“bevingtona” <alexandre.bevington@gov.bc.ca>
2019-11-08 12:46:57 -0800

    add all layers in training data

--- R/02_clean.R
+++ R/02_clean.R
@@ -29,12 +29,12 @@ for (i in seq(1:length(list.indices))) {
 
 saveRDS(img, "temp/img_indices.R")
 
-}
+# }
 
 
 
 
-img <- readRDS("temp/img_Data.R")
+img <- readRDS("temp/img_indices.R")
 
 # Convert raster to numeric
 nr <- getValues(img)
@@ -62,7 +62,7 @@ ggR(knr, forceCat = T, geom_raster = T) +
 xy <- xyFromCell(img[[1]], kmncluster$cluster)
 xy <- data.frame(xy)
 training <- tibble(class = kmncluster$cluster, x = xy$x, y = xy$y)
-
+training <- cbind(nr, training)
 # Add classification to spectral matrix
 training <- training %>%
   filter(class %in% c(1,2,5))
